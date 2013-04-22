bcf24b75f5634386f199e964fe8f5040be86658f
vince <vince@macroot.(none)>
2013-04-22 10:59:38 +0200

    minor correction in search

--- search.php
+++ search.php
@@ -1,15 +1,28 @@
 <?php
 $keyword='';
 $isKeywordSet=False;
-if(isset($_POST['keyword'])){
-  $keyword=$_POST['keyword'];
-  $isKeywordSet=True;
+if(isset($_GET['keyword'])){
+  if($_GET['keyword']!=''){
+    $keyword=$_GET['keyword'];
+    $isKeywordSet=True;
+  }
 }
 ?>
 
 <header class="jumbotron subhead" id="overview">
   <h2>Search</h2>
-  <form class="form-horizontal" action="index.php?page=search" method="post">
+  <form class="form-horizontal" action="index.php?page=search" method="get">
+    <?php
+    $keys = array('page');
+    foreach($keys as $name) {
+      if(!isset($_GET[$name])) {
+        continue;
+      }
+      $value = htmlspecialchars($_GET[$name]);
+      $name = htmlspecialchars($name);
+      echo '<input type="hidden" name="'. $name .'" value="'. $value .'">';
+    }
+    ?>
     <fieldset>
       <legend>Query the database with a keyword</legend>
       <div class="control-group">
@@ -29,27 +42,27 @@ if(isset($_POST['keyword'])){
 if(!$isKeywordSet){
   echo '<!--';
 } else {
-  $searchkey = $_POST['keyword'];
+  $searchkey = $_GET['keyword'];
 }
 ?>
 
 <div class="tabbable">
-    <ul class="nav nav-tabs">
-        <li class="active"><a href="#athlete" data-toggle="tab">Athletes</a></li>
-        <li><a href="#olympics" data-toggle="tab">Olympics</a></li>
-        <li><a href="#countries" data-toggle="tab">Countries</a></li>
-    </ul>
-    <div class="tab-content">
-        <div class="tab-pane active" id="athlete">
-        <?php include('search_tables/athletes_table.php'); ?>
-      </div>
-        <div class="tab-pane" id="olympics">
-        <?php include('search_tables/olympics_table.php'); ?>
-        </div>
-        <div class="tab-pane" id="countries">
-        <?php include('search_tables/countries_table.php'); ?>
-        </div>
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#athlete" data-toggle="tab">Athletes</a></li>
+    <li><a href="#olympics" data-toggle="tab">Olympics</a></li>
+    <li><a href="#countries" data-toggle="tab">Countries</a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="athlete">
+      <?php if($isKeywordSet) include('search_tables/athletes_table.php'); ?>
     </div>
+    <div class="tab-pane" id="olympics">
+      <?php if($isKeywordSet) include('search_tables/olympics_table.php'); ?>
+    </div>
+    <div class="tab-pane" id="countries">
+      <?php if($isKeywordSet) include('search_tables/countries_table.php'); ?>
+    </div>
+  </div>
 </div>
 
 <?php
