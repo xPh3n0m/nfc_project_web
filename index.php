a007f3f64de941001aa36ead954c555c007dc132
demarign <vincent.demarignac@epfl.ch>
2013-04-23 15:55:18 +0200

    added favicon

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
   <head>
     <meta charset="utf-8">
     <title>Olympics 14</title>
+    <link rel="icon" href="img/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Olympics 14 is a Introduction to Database Systems project from group #14">
     <meta name="author" content="">
+
     <link href="css/bootstrap.css" rel="stylesheet">
     <style>
       body {
