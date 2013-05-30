0dc15cc70a55941cd574c7ca5528e80e5690e41c
vince <vince@macroot.(none)>
2013-05-30 15:40:35 +0200

    post pas postE, toclet

--- search.php
+++ search.php
@@ -1,10 +1,30 @@
 <?php
+$query='';
+$isQuerySet=False;
 $keyword='';
 $isKeywordSet=False;
 if(isset($_GET['k'])){
   if($_GET['k']!=''){
     $keyword=$_GET['k'];
     $isKeywordSet=True;
+  }
+}elseif(isset($_POST['q'])){
+  if($_POST['q']!=''){
+    $query=$_POST['q'];
+    $isQuerySet = True;
+  }
+}
+
+// This function ensures that entered fields are correctly restored
+function restore_fields(){
+  $keys = array('p');
+  foreach($keys as $name) {
+    if(!isset($_GET[$name])) {
+      continue;
+    }
+    $value = htmlspecialchars($_GET[$name]);
+    $name = htmlspecialchars($name);
+    echo '<input type="hidden" name="'. $name .'" value="'. $value .'">';
   }
 }
 ?>
@@ -13,23 +33,14 @@ if(isset($_GET['k'])){
   <h2>Search</h2>
   <div class="tabbable">
     <ul class="nav nav-pills">
-      <li class="active"><a href="#keyword" data-toggle="tab">Keyword</a></li>
-      <li><a href="#query" data-toggle="tab">Query</a></li>
+      <li class="disabled"><a href="#">Input method:</a></li>
+      <li <?php if(!$isQuerySet) echo "class=\"active\"" ?>><a href="#keyword" data-toggle="tab">Keyword</a></li>
+      <li <?php if($isQuerySet) echo "class=\"active\"" ?>><a href="#query" data-toggle="tab">Query</a></li>
     </ul>
-    <div class="pill-content">
-      <div class="tab-pane active" id="keyword">
+    <ul class="tab-content">
+      <li class="tab-pane <?php if(!$isQuerySet) echo "active"; ?>" id="keyword">
         <form class="form-horizontal" action="index.php?p=search" method="get">
-          <?php
-          $keys = array('p');
-          foreach($keys as $name) {
-            if(!isset($_GET[$name])) {
-              continue;
-            }
-            $value = htmlspecialchars($_GET[$name]);
-            $name = htmlspecialchars($name);
-            echo '<input type="hidden" name="'. $name .'" value="'. $value .'">';
-          }
-          ?>
+          <?php restore_fields(); ?>
           <fieldset>
             <legend>Search the database with a keyword</legend>
             <div class="control-group">
@@ -43,43 +54,65 @@ if(isset($_GET['k'])){
             </div>
           </fieldset>
         </form>
-      </div>
-      <div class="tab-pane" id="query">
-        Salut ça va?!
-      </div>
-    </div>
+
+        <?php
+        if(!$isKeywordSet){
+          echo '<!--';
+        } else {
+          $searchkey = $_GET['k'];
+        }
+        ?>
+
+        <div class="tabbable">
+          <ul class="nav nav-tabs">
+            <li class="active"><a href="#athlete" data-toggle="tab">Athletes</a></li>
+            <li><a href="#olympics" data-toggle="tab">Olympics</a></li>
+            <li><a href="#countries" data-toggle="tab">Countries</a></li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="athlete">
+              <?php if($isKeywordSet) include('search_tables/athletes_table.php'); ?>
+            </div>
+            <div class="tab-pane" id="olympics">
+              <?php if($isKeywordSet) include('search_tables/olympics_table.php'); ?>
+            </div>
+            <div class="tab-pane" id="countries">
+              <?php if($isKeywordSet) include('search_tables/countries_table.php'); ?>
+            </div>
+          </div>
+        </div>
+
+        <?php
+        if(!$isKeywordSet){
+          echo '-->'; 
+        }
+        ?>
+
+      </li>
+      <li class="tab-pane <?php if($isQuerySet) echo "active"; ?>" id="query">
+        
+        <form class="form-horizontal" action="index.php?p=search" method="post">
+          <?php restore_fields(); ?>
+          <fieldset>
+            <legend>Here you can type a search query</legend>
+            <div class="control-group">
+              <label class="control-label" for="query">Query:</label>
+              <div class="controls">
+                <textarea name="q" id="query" cols="30" rows="10"></textarea>
+              </div>
+            </div>
+            <div class="form-actions">
+              <button id="btn-query" type="submit" class="btn btn-primary">Run query</button>
+            </div>
+          </fieldset>
+        </form>
+
+        <?php
+        if($isQuerySet){
+          echo $query;
+        }
+        ?>
+      </li>
+    </ul>
   </div>
 </header>
-
-<?php
-if(!$isKeywordSet){
-  echo '<!--';
-} else {
-  $searchkey = $_GET['k'];
-}
-?>
-
-<div class="tabbable">
-  <ul class="nav nav-tabs">
-    <li class="active"><a href="#athlete" data-toggle="tab">Athletes</a></li>
-    <li><a href="#olympics" data-toggle="tab">Olympics</a></li>
-    <li><a href="#countries" data-toggle="tab">Countries</a></li>
-  </ul>
-  <div class="tab-content">
-    <div class="tab-pane active" id="athlete">
-      <?php if($isKeywordSet) include('search_tables/athletes_table.php'); ?>
-    </div>
-    <div class="tab-pane" id="olympics">
-      <?php if($isKeywordSet) include('search_tables/olympics_table.php'); ?>
-    </div>
-    <div class="tab-pane" id="countries">
-      <?php if($isKeywordSet) include('search_tables/countries_table.php'); ?>
-    </div>
-  </div>
-</div>
-
-<?php
-if(!$isKeywordSet){
-  echo '-->';
-}
-?>
