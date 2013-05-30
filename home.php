c37661660471940ca8a467659ea6d9702a7e3edc
vince <vince@macroot.(none)>
2013-05-30 22:17:56 +0200

    page robustness

--- home.php
+++ home.php
@@ -1,3 +1,6 @@
+<?php
+if(!isset($isReferencing)) header('Location: index.php');
+?>
 <h2>Welcome to Olympics 14!</h2>
 <p>
 This web front-end application serves the purpose of interacting with the Olympic Games' data.<br>
