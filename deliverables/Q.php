c616c5681f3b584a55c50dd696611052a769176c
nyfeler <thierry.nyfeler@epfl.ch>
2013-05-31 19:07:58 +0200

    Added deliverables Q-T

--- deliverables/Q.php
+++ deliverables/Q.php
@@ -1,1 +1,18 @@
-Not yet available
+<?php
+$header = 'Q. For each Olympic Games, list the name of the country which scored the largest percentage of the 
+medals.';
+
+$sql = 'select t1.olympics, t2.country, t1.m/t1.s as percentage
+from (select t.olympics, max(t.medals) as m, sum(t.medals) as s
+      from (select m.olympics, count(*) as medals, row_number() over (partition by m.olympics order by count(*) desc) as n
+            from (select distinct medal, olympics, country, sport, disciplines from medals) m
+            group by m.olympics, m.country) t
+      group by olympics) t1,
+     (select m.olympics, m.country, count(*) as medals
+      from (select distinct medal, olympics, country, sport, disciplines from medals) m
+      group by m.olympics, m.country) t2
+where t1.m=t2.medals and t1.olympics=t2.olympics
+order by t1.olympics';
+
+$columns = array('Olympics', 'Country', 'Percentage');
+?>
--- deliverables/S.php
+++ deliverables/S.php
@@ -1,1 +1,19 @@
-Not yet available
+<?php
+$header = 'S. List names of all athletes who won medals both in individual and team sports.';
+
+$sql = 'select a.name
+from athletes a
+where a.aid in (select distinct m.aid
+                from medals m
+                where (m.aid, m.country, m.olympics, m.sport) in (select aid, country, olympics, sport
+                                                                  from participants
+                                                                  where team_name is null)
+                intersect
+                select distinct m.aid
+                from medals m
+                where (m.aid, m.country, m.olympics, m.sport) in (select aid, country, olympics, sport
+                                                                  from participants
+                                                                  where team_name is not null))';
+
+$columns = array('Athlete Name');
+?>
