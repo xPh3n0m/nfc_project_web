013dbe2fbe187e40cdfa85decc0ccbf769e222ec
vince <vince@macroot.(none)>
2013-06-02 03:07:57 +0200

    bug correction on insert_athlete

--- insert_data/insert_athlete.php
+++ insert_data/insert_athlete.php
@@ -1,32 +1,36 @@
 <?php
 if(!isset($isReferencing)) header('Location: ../index.php');
-$athlete_name = $_POST['name'];
+if(isset($_POST['name'])){
+	$athlete_name = $_POST['name'];
+	if($athlete_name != ''){
 
-$conn = oci_connect('db2013_g14', 'gwathivin', '//icoracle.epfl.ch:1521/srso4.epfl.ch');
+		$conn = oci_connect('db2013_g14', 'gwathivin', '//icoracle.epfl.ch:1521/srso4.epfl.ch');
 
-if (!$conn) {
-  $e = oci_error();
-  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		if (!$conn) {
+			$e = oci_error();
+			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+
+		$stid = oci_parse(
+			$conn, "INSERT INTO Athletes
+			VALUES((SELECT MAX(aid) FROM Athletes A) + 1, '" . $athlete_name . "')" 
+		);
+
+		if (!$stid) {
+			$e = oci_error($conn);
+			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+
+		$r = oci_execute($stid);
+		if (!$r) {
+			$e = oci_error($stid);
+			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+
+		oci_free_statement($stid);
+		oci_close($conn);
+	}
 }
-
-$stid = oci_parse(
-  $conn, "INSERT INTO Athletes
-  VALUES((SELECT MAX(aid) FROM Athletes A) + 1, '" . $athlete_name . "')" 
-);
-
-if (!$stid) {
-  $e = oci_error($conn);
-  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
-
-$r = oci_execute($stid);
-if (!$r) {
-  $e = oci_error($stid);
-  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
-
-oci_free_statement($stid);
-oci_close($conn);
 
 header('Location: ../index.php?p=insert');
 ?>
