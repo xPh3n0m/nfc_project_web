93845a7eb985efc8ceae589654548bc4802698b8
Thierry Nyfeler <thierry.nyfeler@gmail.com>
2016-01-11 17:59:11 +0100

    Register wristband now works

--- nfc_app/index.js
+++ nfc_app/index.js
@@ -17,6 +17,21 @@ app.set('view engine', 'php');
 // routing all .php file to php-express
 app.all(/.+\.php$/, phpExpress.router);
 
+// Connect to PG database
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgresql://root:nfcproject@nfcprojectinstance.cpx69rsmkoux.us-west-2.rds.amazonaws.com:5432/nfcprojectdb';
+
+var rollback = function(client, done) {
+  client.query('ROLLBACK', function(err) {
+    //if there was a problem rolling back the query
+    //something is seriously messed up.  Return the error
+    //to the done function to close & remove this client from
+    //the pool.  If you leave a client in the pool with an unaborted
+    //transaction weird, hard to diagnose problems might happen.
+    return done(err);
+  });
+};
+
 app.get('/nfc_app', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -29,9 +44,74 @@ io.on('connection', function(socket){
 
   socket.on('nfc_card_connected', function(data) {
     console.log('sending room post', data.room);
-    socket.broadcast.to(data.room).emit('nfc_card_connected_message', {
-        message: data.message
+    var results = [];
+    console.log(data);
+
+    pg.connect(connectionString, function(err, client, done) {
+      if(err) {
+        return console.error('error fetching client from pool', err);
+      }
+      client.query('BEGIN', function(err) {
+        if(err) return rollback(client, done);
+        process.nextTick(function() {
+          var selectWristbandQuery = 'SELECT w.wid, w.balance, w.status, w.uid FROM wristband w WHERE w.uid = $1::bytea';
+          client.query(selectWristbandQuery, [data.message], function(err, result) {
+            if(err) return rollback(client, done);
+            var wristband = {};
+            //Check if the wristband is already logged in the database
+            if(typeof result.rows[0] != 'undefined') {
+              wristband=result.rows[0];
+              if(wristband.gid>0) {
+                var selectGuestQuery = 'SELECT g.first_name, g.last_name, g.email, g.anonymous FROM guest g WHERE g.gid = $1';
+                client.query(selectGuestQuery, wristband.gid, function(err, res) {
+                  if(err) return rollback(client, done);
+                  if(typeof res.rows[0] != 'undefined') {
+                    wristband.first_name=res.rows[0].first_name;
+                    wristband.last_name=res.rows[0].last_name;
+                    wristband.email=res.rows[0].email;
+                    wristband.anonymous=res.rows[0].anonymous;
+                  }
+
+                });
+              }
+            } else {
+              wristband = data;
+              wristband.uid=data.message;
+              delete wristband.message;
+            }
+            console.log("Wrisband sent to client: " + wristband.uid);
+            socket.broadcast.to(data.room).emit('nfc_card_connected_message', {
+              message: wristband
+            });
+          });
+        });
+      });
     });
+/*
+      client.query('SELECT w.wid, w.balance, g.gid, g.first_name, g.last_name, g.email, g.anonymous from wristband w, guest g WHERE w.uid = $1::bytea AND w.gid = g.gid', [data.message], function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+        //console.log(result.rows[0]);
+        //output: 1
+        var wristband = {};
+        if(typeof result.rows[0] != 'undefined') {
+          wristband=result.rows[0];
+        } else {
+          data.balance=0.0;
+          data.gid=-1;
+          data.status='U';
+          wristband = registerWristband(data);
+        }
+
+        console.log(wristband);
+        socket.broadcast.to(data.room).emit('nfc_card_connected_message', {
+          message: wristband
+        });
+      });*/
   });
 
   socket.on('nfc_card_disconnected', function(data) {
@@ -40,18 +120,38 @@ io.on('connection', function(socket){
         message: data.message
     });
   });
-/*
-  socket.on('nfc_card_connected', function(msg){
-  	console.log("NFC Card connected. UID: " + msg);
-    io.emit('nfc_card_connected', msg);
-  });
 
-  socket.on('nfc_card_disconnected', function (msg) {
-    console.log("NFC Card disconnected. UID: " + msg);
-    io.emit('nfc_card_disconnected', msg);
-  });*/
+  socket.on('register_wristband', function(data) {
+    var regData = registerWristband(data);
+    socket.broadcast.to(data.room).emit('register_wristband_succesful', {
+      message: regData
+    });
+  });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function registerWristband(data) {
+    console.log("Data to register: " + data.gid);
+
+    pg.connect(connectionString, function(err, client, done) {
+      if(err) {
+        return console.error('error fetching client from pool', err);
+      }
+      client.query('INSERT INTO wristband (uid, balance, gid, status) VALUES ($1::bytea, $2, $3, $4) RETURNING wid;', [data.uid, data.balance, data.gid, data.status], function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows[0]);
+
+        data.wid = result.rows[0].wid;
+        console.log('Succesfully registered new wristband ' + data);
+        return data;
+      });
+    });
+  }
--- nfc_app/index.php
+++ nfc_app/index.php
@@ -5,17 +5,23 @@ if(!isset($isReferencing)) header('Location: index.php');
 <script src="https://cdn.socket.io/socket.io-1.4.3.js"></script>
 <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
 <script>
+var socket;
+var currentMessage;
   $( document ).ready(function() {
-      var socket = io.connect('http://localhost:3000');
-      var sessionid = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+      socket = io.connect('http://localhost:3000');
+      var sessionid = "sessionid";
+
       $('#sid').html(sessionid);
       socket.emit('subscribe', sessionid);
 
       socket.on('nfc_card_connected_message', function(msg){
-        $('#wid').val(msg.message);
-      });
+          currentMessage = msg.message;
+          handleNfcConnectionMessage(msg.message);
+        });
+
       socket.on('nfc_card_disconnected_message', function(msg){
-        $('#wid').val('');
+          currentMessage = null;
+          handleNfcDisconnectionMessage(msg.message);
       });
   });
 </script>
@@ -25,13 +31,111 @@ if(!isset($isReferencing)) header('Location: index.php');
 <section class="container content">
   <fieldset>
     <legend>NFC Application</legend>
+    <div visibility="hidden" id="uid"></div>
     <div class="control-group">
-      <label class="control-label" for="wid">Current Wristband ID</label>
+      <label class="control-label" for="wid">Wristband ID</label>
       <div class="controls">
-        <input type="text" class="input-xlarge required" name="wid" id="wid">
+        <input type="text" disabled="disabled" class="input-xlarge required" name="wid" id="wid">
       </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="balance">Balance</label>
+      <div class="controls">
+        <input type="text" disabled="disabled" class="input-xlarge required" name="balance" id="balance">
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="first_name">First Name</label>
+      <div class="controls">
+        <input type="text" disabled="disabled" class="input-xlarge required" name="first_name" id="first_name">
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="last_name">Last Name</label>
+      <div class="controls">
+        <input type="text" disabled="disabled" class="input-xlarge required" name="last_name" id="last_name">
+      </div>
+    </div>
+
+    <div class="control-group">
+      <label class="control-label" for="email">Email</label>
+      <div class="controls">
+        <input type="text" disabled="disabled" class="input-xlarge required" name="email" id="email">
+      </div>
+    </div>
+    <div class="control-group">
+      <input type="checkbox" disabled="disabled" id="anonymous" value="anonymous"> Anonymous?<br>
     </div>
   </fieldset>
 </section>
+
+<button onclick="registerWristband()" class="btn btn-primary" disabled="disabled" type="button" id="reg_wristband_button">Register Wristband</button>
+<button class="btn btn-primary" disabled="disabled" type="button" id="reg_guest_button">Register new Guest</button>
+<button class="btn btn-primary" disabled="disabled" type="button" id="update_guest_button">Update Guest</button>
 </body>
+
+
+<script>
+  function registerWristband() {
+    var data = {};
+    data.gid=-1;
+    data.balance=0.0;
+    data.uid=currentMessage.uid;
+    data.status='I';
+    socket.emit('register_wristband', data);
+  }
+
+  function handleNfcConnectionMessage(message) {
+    $('#uid').html(message.uid);
+    if (typeof message.wid == 'undefined') {
+      $('#reg_wristband_button').removeAttr('disabled');
+    } else {
+      $('#wid').val(message.wid);
+      $('#balance').val(message.balance);
+      
+      if(typeof message.gid == 'undefined') {
+        $('#reg_guest_button').removeAttr('disabled');
+      } else {
+        $('#first_name').val(message.first_name);
+        $('#last_name').val(message.last_name);
+        $('#email').val(message.email);
+        if(message.anonymous) {
+          $('#anonymous').attr('checked', true);
+        }
+
+        $('#first_name').removeAttr('disabled');
+        $('#last_name').removeAttr('disabled');
+        $('#email').removeAttr('disabled');
+        $('#first_name').removeAttr('disabled');
+        $('#anonymous').removeAttr('disabled');
+
+        $('#update_guest_button').removeAttr('disabled');
+      }
+    }
+  }
+
+  function handleNfcDisconnectionMessage(message) {
+    $('#uid').val('');
+    $('#wid').val('');
+    $('#balance').val('');
+    $('#first_name').val('');
+    $('#last_name').val('');
+    $('#email').val('');
+    $('#anonymous').attr('checked', false);
+
+    $('#first_name').attr('disabled','disabled');
+    $('#last_name').attr('disabled','disabled');
+    $('#email').attr('disabled','disabled');
+    $('#first_name').attr('disabled','disabled');
+    $('#anonymous').attr('disabled','disabled');
+
+    $('#reg_wristband_button').attr('disabled','disabled');
+    $('#reg_guest_button').attr('disabled','disabled');
+    $('#update_guest_button').attr('disabled','disabled');
+  }
+</script>
+
 
